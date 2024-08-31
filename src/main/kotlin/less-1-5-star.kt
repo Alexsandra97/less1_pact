806fe3cc468723148c9bbdedc564d5cc0cf3d10a
Alexsandra97 <thefirstone010101@gmail.com>
2024-08-31 22:00:56 +1000

    less-1-5 add space

--- src/main/kotlin/less-1-5-star.kt
+++ src/main/kotlin/less-1-5-star.kt
@@ -1,12 +1,12 @@
 package org.example
 
-fun main(){
+fun main() {
 
     val timeConst: Int = 60
     val seconds: Int = 6480
-    val minute: Int = (seconds % (timeConst*timeConst)) / timeConst
-    val hour: Int = seconds / (timeConst*timeConst)
-    val secSecond: Int = (seconds % (timeConst*timeConst)) % timeConst
+    val minute: Int = (seconds % (timeConst * timeConst)) / timeConst
+    val hour: Int = seconds / (timeConst * timeConst)
+    val secSecond: Int = (seconds % (timeConst * timeConst)) % timeConst
 
     println(String.format("%02d:%02d:%02d", hour, minute, secSecond))
 }
