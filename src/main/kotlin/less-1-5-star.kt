c6d7b22fa6814bc223db0d4426a6aa892e316c27
Alexsandra97 <thefirstone010101@gmail.com>
2024-09-01 11:44:37 +1000

    less-1-5 const and detach

--- src/main/kotlin/less-1-5-star.kt
+++ src/main/kotlin/less-1-5-star.kt
@@ -2,11 +2,14 @@ package org.example
 
 fun main() {
 
-    val timeConst: Int = 60
+    val TIME = 60
+
     val seconds: Int = 6480
-    val minute: Int = (seconds % (timeConst * timeConst)) / timeConst
-    val hour: Int = seconds / (timeConst * timeConst)
-    val secSecond: Int = (seconds % (timeConst * timeConst)) % timeConst
+
+    val minute: Int = (seconds % (TIME * TIME)) / TIME
+    val hour: Int = seconds / (TIME * TIME)
+    val secSecond: Int = (seconds % (TIME * TIME)) % TIME
+
 
     println(String.format("%02d:%02d:%02d", hour, minute, secSecond))
 }
