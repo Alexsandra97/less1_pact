135644831ca2260fed25644530cef61195b78fa5
Alexsandra97 <thefirstone010101@gmail.com>
2024-09-01 11:54:05 +1000

    less-2-1- add float and change name

--- src/main/kotlin/less-2-1-gpa.kt
+++ src/main/kotlin/less-2-1-gpa.kt
@@ -2,12 +2,12 @@ package org.example
 
 fun main() {
 
-    val score1 = 3
-    val score2 = 4
-    val score3 = 3
-    val score4 = 5
-    val sumStudents: Float = 4f
-    val gpa = (score1 + score2 + score3 + score4) / sumStudents
+    val scoreStudent1: Float = 3f
+    val scoreStudent2: Float = 4f
+    val scoreStudent3: Float = 3f
+    val scoreStudent4: Float = 5f
+    val sumStudents = 4f
+    val gpa = (scoreStudent1 + scoreStudent2 + scoreStudent3 + scoreStudent4) / sumStudents
 
     println("Средний балл: $gpa")
 }
